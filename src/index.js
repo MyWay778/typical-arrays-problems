57b23e43e7897ee0c14b14dfec0864b1943da2ce
My name <sakla4onakne@mail.ru>
2021-01-14 11:27:10 +0400

    solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0
+  }
+  let minValue = array[0] 
+  for (let i = 0; i < array.length; i++) {
+    if (minValue > array[i]) {
+      minValue = array[i]
+    }
+  }
+  return minValue
 }
 
 exports.max = function max (array) {
+  if (array && array.length) {
+    return Math.max(...array)
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length) {
+    return array.reduce((sum, current) => sum + current ) / array.length
+  }
   return 0;
 }
